696635fbab1cae4857bab499db320d4ccedbf017
Laknath1996 <ashwindesilva@yahoo.com>
2023-10-12 09:36:58 +0000

    Deploying to gh-pages from @ Laknath1996/laknath1996.github.io@c349333d9431836928d0d6c00fc95bbf9e2d4ee0 🚀

---
+++ cv/build.sh
@@ -0,0 +1,9 @@
+# pdflatex main
+# bibtex main
+# bibtex conference
+# bibtex workshop_papers
+# bibtex preprints
+# bibtex theses
+# pdflatex main
+# pdflatex main
+cp cv/main.pdf /Users/ashwindesilva/CV/ashwin_cv.pdf
